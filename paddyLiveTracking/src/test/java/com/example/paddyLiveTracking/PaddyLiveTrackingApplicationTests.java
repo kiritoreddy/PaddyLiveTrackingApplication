ef7af0ef856ce448edeed1d21e526cf29589f7c6
vazrala vinith reddy <vvr.vazrala@gmail.com>
2024-10-09 01:04:46 +0530

    initial commit

---
+++ paddyLiveTracking/src/test/java/com/example/paddyLiveTracking/PaddyLiveTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paddyLiveTracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaddyLiveTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
