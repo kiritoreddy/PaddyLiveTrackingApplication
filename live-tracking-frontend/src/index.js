3c2b20195f0e76cd989440d6a43df05b8ed359cf
vazrala vinith reddy <vvr.vazrala@gmail.com>
2024-10-15 18:26:51 +0530

    fixed initial issues

--- live-tracking-frontend/src/index.js
+++ live-tracking-frontend/src/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(<BrowserRouter><AuthProvider><App /></AuthProvider></BrowserRouter>, document.getElementById('root'));
